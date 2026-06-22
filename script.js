60756f30e3361bf725dd07734bffc0328189a2c5
Soubhik Sarkar <soubhiksarkar226@gmail.com>
2026-06-22 08:32:19 +0530

    add local storage

--- script.js
+++ script.js
@@ -6,27 +6,21 @@ const completed = document.querySelector(".completed");
 const edit = document.querySelector(".edit");
 const delete_ = document.querySelector(".delete");
 const displayParent = document.querySelector(".displayParent");
-
-let completeTask = 0;
-let totalTask = 0;
-
-
-
-
-let pendingTask = totalTask-completeTask;
-// console.log(taskInput);
+const outerEditDiv = document.querySelector(".outer-edit-div");
+const cross = document.querySelector
 
 let task=JSON.parse(localStorage.getItem("task")) || [];
-console.log(task);
+
 
 function ui(){
     cardArea.innerHTML ="";
     task.forEach((elem,idx)=>{
     cardArea.innerHTML += `<div class="card">
             <p>${elem.input} ${elem.purpose}</p>
+            <div class="status">${elem.status} </div>
             <div class="btn">
             <button onclick="completedfunc(${idx})" class="completed">Completed</button>
-            <button class="edit">Edit</button>
+            <button onclick="editfunc(${idx})" class="edit">Edit</button>
             <button onclick="deletefunc(${idx})" class="delete">Delete</button>
             </div>
         </div>`;
@@ -34,6 +28,9 @@ function ui(){
 }
 
 function updateDashboard(){
+        let totalTask = task.length;
+        let completeTask = task.filter(a=> a.status === "Completed").length;
+        let pendingTask = task.filter(a=> a.status === "Pending").length;
         displayParent.innerHTML="";
         displayParent.innerHTML+=`
         <div class="display">
@@ -46,7 +43,7 @@ function updateDashboard(){
           <p>Completed task</p>
         </div>
         <div class="right">
-          <p class="pending">${totalTask-completeTask}</p>
+          <p class="pending">${pendingTask}</p>
           <p>Pending task</p>
         </div>
         </div>`
@@ -63,35 +60,70 @@ form.addEventListener("submit",(e)=>{
     task.push({
         input,
         purpose,
+        status:"Pending"
     })
     localStorage.setItem("task",JSON.stringify(task));
-    totalTask++;
+
+    
     updateDashboard();
     ui();
 });
 
 function completedfunc(idx){
     
-        task.splice(idx,1);
+        task[idx].status = "Completed";
         localStorage.setItem("task",JSON.stringify(task));
-        completeTask++;
         updateDashboard();
         ui();
 }
 
 function deletefunc(idx){
-        totalTask--;
-        updateDashboard();
         task.splice(idx,1);
         localStorage.setItem("task",JSON.stringify(task));
+        updateDashboard();
         ui();
 }
 
-// function editfunc(idx){
-    
-//         arr.splice(idx,1);
-//         ui();
-// }
+document.addEventListener("click",(e)=>{
+        if(e.target.classList.contains("cross")){
+                outerEditDiv.style.display = "none";
+        }
+}
+)
+
+function editfunc(idx){
+        outerEditDiv.style.display="block";
+        outerEditDiv.innerHTML=` 
+        <div class="edit-div">
+        <div class="cross">X</div>
+        <form action="" class="edit-form">
+                <input type="text" class="edit-input" value="${task[idx].input}">
+                <select name="dropdown" class="edit-purpose" id="dropdown">
+                 <option value="${task[idx].purpose}">${task[idx].purpose} </option>
+                 <option value="Personal">Personal</option>
+                 <option value="Work">Work</option>
+                 <option value="Study">Study</option>
+                 <option value="Other">Other</option>
+                </select>
+                <button class="edit-button" data-id="${idx}">Edit</button>
+          
+        </form>
+        </div>
+        </div>`
+}
+
+document.addEventListener("click",(e)=>{
+        if(e.target.classList.contains("edit-button")){
+                let idx = e.target.dataset.id;
+                let editInput = document.querySelector(".edit-input");
+                let editPurpose = document.querySelector(".edit-purpose");
+                task[idx].input = editInput.value;
+                task[idx].purpose = editPurpose.value;
+                localStorage.setItem("task",JSON.stringify(task));
+                ui();
+                updateDashboard();
+        }
+})
 
 
 
